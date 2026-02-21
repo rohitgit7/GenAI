e45597417859a860f5f83e524419f72fc20e0ead
rohitgit7 <rohitgit7@users.noreply.github.com>
2026-02-21 19:01:07 +0530

    Initial commit

---
+++ First_GenAI_API_Program.py
@@ -0,0 +1,18 @@
+import openai
+import os
+
+client = openai.OpenAI(
+    api_key = os.getenv("POE_API_KEY"),
+    base_url = "https://api.poe.com/v1"
+)
+
+chat = client.chat.completions.create(
+    model = "gemini-2.0-flash",
+    messages = [{
+      "role": "user",
+      "content": "can I login to claude desktop using poe credentials?"
+    }]
+
+)
+
+print(chat.choices[0].message.content)
